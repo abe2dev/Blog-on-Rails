6e065d6dd64d9dce8df35ba78842cf7ddf4c7b8a
Ibrahim <98372964+abe2dev@users.noreply.github.com>
2022-03-13 22:55:56 -0700

    Create Posts controller, views and all RESTful routes

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,49 @@
+class PostsController < ApplicationController
+    def index 
+        @posts = Post.all
+    end
+    
+    def show
+        @post = Post.find params[:id]
+    rescue => e
+        redirect_to posts_path, alert: e.message 
+    end 
+
+    def destroy
+        @post = Post.find params[:id]
+        @post.destroy
+        redirect_to posts_path, { notice: "Post deleted successfully", status: 303 }
+    rescue => e
+        redirect_to posts_path, alert: e.message
+    end 
+
+    def new
+        @post = Post.new
+    end
+
+    def create
+        @post = Post.new params.require(:post).permit(:title, :body)
+        if @post.save
+            redirect_to post_path(@post)
+        else
+            render :new, status: 303
+        end
+    end
+
+    def edit
+        @post = Post.find params[:id]
+    rescue => e
+        redirect_to posts_path, alert: e.message
+    end
+
+    def update
+        @post = Post.find params[:id]
+        if @post.update params.require(:post).permit(:title, :body)
+            redirect_to post_path(@post)
+        else
+            render :edit, status:303
+        end
+    rescue => e
+        redirect_to posts_path, alert: e.message
+    end
+end
