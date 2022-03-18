d521542c5f38ac5c0e3eaf899c8ef0d0411ac8a0
Ibrahim <98372964+abe2dev@users.noreply.github.com>
2022-03-18 14:06:01 -0700

    Semantic HTML elements and CSS

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,51 +1,55 @@
+# postsController
 class PostsController < ApplicationController
-    def index 
-        @posts = Post.order(created_at: :desc)
+    def index
+      @posts = Post.order(created_at: :desc)
     end
-    
+  
     def show
-        @post = Post.find params[:id]
-        @comment = Comment.new
-        @comments = @post.comments.order(created_at: :desc)
-    rescue => e
-        redirect_to root_path, alert: e.message 
-    end 
-
+      @post = Post.find params[:id]
+      @comment = Comment.new
+      @comments = @post.comments.order(created_at: :desc)
+    rescue StandardError => e
+      redirect_to root_path, alert: e.message
+    end
+  
     def destroy
-        @post = Post.find params[:id]
-        @post.destroy
-        redirect_to root_path, { notice: "Post deleted successfully", status: 303 }
-    rescue => e
-        redirect_to root_path, alert: e.message
-    end 
-
+      @post = Post.find params[:id]
+      @post.destroy
+      redirect_to posts_path, { notice: 'Post deleted successfully', status: 303 }
+    rescue StandardError => e
+      redirect_to root_path, alert: e.message
+    end
+  
     def new
-        @post = Post.new
+      @post = Post.new
     end
-
+  
     def create
-        @post = Post.new params.require(:post).permit(:title, :body)
-        if @post.save
-            redirect_to post_path(@post)
-        else
-            render :new, status: 303
-        end
+      @post = Post.new params.require(:post).permit(:title, :body)
+      if @post.save
+        redirect_to post_path(@post) #{ status: 303, notice: 'Post created successfully' }
+      else
+        render :new, status: 303
+      end
     end
-
+  
     def edit
-        @post = Post.find params[:id]
-    rescue => e
-        redirect_to root_path, alert: e.message
+      @post = Post.find params[:id]
+    rescue StandardError => e
+      redirect_to root_path, alert: e.message
     end
-
+  
     def update
-        @post = Post.find params[:id]
-        if @post.update params.require(:post).permit(:title, :body)
-            redirect_to post_path(@post), { notice: "Post updated successfully", status: 303 }
-        else
-            render :edit, status:303
-        end
-    rescue => e
-        redirect_to root_path, alert: e.message
+      @post = Post.find params[:id]
+      puts @post
+      if @post.update params.require(:post).permit(:title, :body)
+        puts @post
+        redirect_to post_path(@post), { status: 303, notice: 'Post updated successfully' }
+      else
+        render :edit, status: 303
+      end
+    rescue StandardError => e
+      redirect_to posts_path, alert: e.message
     end
-end
+  end
+  
