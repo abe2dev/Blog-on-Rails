0f993a51773c9bc2c3c97458d646f3adfc534b97
Ibrahim MA <ibrahimood1@gmail.com>
2022-03-23 00:15:58 -0700

    Edit Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :authenticated_user!, only: [:edit, :update]
   def new
     @user = User.new
   end
@@ -14,5 +15,15 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find_by_id params[:id]
+  end
+
+  def update
+    @user = User.find_by_id params[:id]
+    if @user.update params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      redirect_to root_path, status: 303
+    else
+      flash.alert = "Update failed, please try again"
+      render :edit, status: 303
+    end
   end
 end
